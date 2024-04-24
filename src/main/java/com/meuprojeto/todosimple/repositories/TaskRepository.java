3e21ac31955081bc97cff3c7c51da56bae1362e2
zBragaTo <leonardobragatobassi@hotmail.com>
2024-04-23 23:12:31 -0300

    Create Repository to Task

---
+++ src/main/java/com/meuprojeto/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,21 @@
+package com.meuprojeto.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.meuprojeto.todosimple.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+		
+	 List<Task> findByUser_Id(Long id);
+	
+	 // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+	// List<Task> findByUserId(@Param("id") Long id);
+	
+	// @Query(value = "SELECT * FROM task t WHERE t.user_id = :id",nativeQuery = true)
+	// List<Task> findByUserId(@Param("id") Long id);
+	
+}
--- src/main/java/com/meuprojeto/todosimple/repositories/UserRepository.java
+++ src/main/java/com/meuprojeto/todosimple/repositories/UserRepository.java
@@ -8,5 +8,5 @@ import com.meuprojeto.todosimple.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	
 }
