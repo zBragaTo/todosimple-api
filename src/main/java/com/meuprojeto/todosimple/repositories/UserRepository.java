a23ced6527b99d0433df0b19574fa815bdce43a4
zBragaTo <leonardobragatobassi@hotmail.com>
2024-04-23 22:56:37 -0300

    Add Repository

---
+++ src/main/java/com/meuprojeto/todosimple/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.meuprojeto.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.meuprojeto.todosimple.models.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
